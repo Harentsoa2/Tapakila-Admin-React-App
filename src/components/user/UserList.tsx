5470cca31ab4dd5fc7ee82a31f5f4fd4018de08a
AinaElie <elie.anjarafanomezana@gmail.com>
2025-03-22 07:29:01 +0300

    feat: ressource user

--- src/components/user/UserList.tsx
+++ src/components/user/UserList.tsx
@@ -1,16 +1,14 @@
-import { Box } from "@mui/material";
-import { Datagrid, DateField, ImageField, List, TextField } from "react-admin";
+import { Avatar } from "@mui/material";
+import { Datagrid, DateField, List, TextField } from "react-admin";
 
 export const UserList = () => (
   <List>
     <Datagrid>
-      <Box sx={{width: "50%", height: "50%"}}>
-        <ImageField source="user_image" label="Profil" />
+        <Avatar src="user_image" aria-label="Profil" />
         <TextField source="id" />
         <TextField source="user_name" label="Nom" />
         <TextField source="user_email" label="Email" />
         <DateField source="user_first_login_date" label="Date de creation" />
-      </Box>
     </Datagrid>
   </List>
 );
