b853c2e20d0dfb8937b165a7c550d8734b3efa8d
AinaElie <elie.anjarafanomezana@gmail.com>
2025-04-01 16:34:08 +0300

    feat: nouveau contenu

--- src/components/message/MessageUser.tsx
+++ src/components/message/MessageUser.tsx
@@ -1,5 +1,11 @@
 import { Typography } from "@mui/material";
-import { ArrayField, Datagrid, RichTextField } from "react-admin";
+import {
+  ArrayField,
+  Datagrid,
+  DateField,
+  RichTextField,
+  TextField,
+} from "react-admin";
 
 const CustomEmpty = () => (
   <Typography sx={{ fontFamily: "Poppins", padding: "20px 0", color: "red" }}>
@@ -7,11 +13,27 @@ const CustomEmpty = () => (
   </Typography>
 );
 
+const MyExpand = () => {
+  return <RichTextField source="message_content" />;
+};
+
 export default function MessageUser() {
   return (
     <ArrayField source="messages" label="">
-      <Datagrid bulkActionButtons={false} empty={<CustomEmpty/>}>
-        <RichTextField source="message_content" /> 
+      <Datagrid
+        bulkActionButtons={false}
+        empty={<CustomEmpty />}
+        expand={<MyExpand />}
+      >
+        {/* <RichTextField source="message_content" />  */}
+        <TextField source="message_id" label="ID" />
+        <TextField source="message_subject" label="Titre" />
+        <DateField
+          source="message_date"
+          label="Date de création"
+          showTime
+          sx={{ color: "#0077FF", fontFamily: "Poppins", fontSize: "1rem" }}
+        />
       </Datagrid>
     </ArrayField>
   );
