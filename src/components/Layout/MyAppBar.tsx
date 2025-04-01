609382996ccece86e15e50228be0d007d1653099
AinaElie <elie.anjarafanomezana@gmail.com>
2025-04-01 08:19:37 +0300

    chore: style appbar

--- src/components/Layout/MyAppBar.tsx
+++ src/components/Layout/MyAppBar.tsx
@@ -1,35 +1,29 @@
-import IconButton from "@mui/material/IconButton";
-import { AppBar, useLogout } from "react-admin";
-import { Avatar, Tooltip } from "@mui/material";
+import { AppBar, Logout, UserMenu } from "react-admin";
 import MyToolbar from "../../screen/MyToolbar";
 
 export default function MyAppBar() {
-  const logout = useLogout();
 
   return (
     <AppBar
       toolbar={<MyToolbar />}
       sx={{
-        background: "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5))",
+        background: "linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8))",
         padding: "10px 20px",
         borderBottom: "2px solid #FFBA08",
         borderBottomRightRadius: "20px",
         borderBottomLeftRadius: "20px",
         "& .RaConfigurable-root": {
-          display: "none"
-        }, 
-        "& .MuiSvgIcon-root": {
-          fontSize: "2rem",
-          color: "#FFBA08"
+          display: "none",
         },
-
+        "& .MuiSvgIcon-fontSizeMedium": {
+          fontSize: "2rem",
+          color: "#FFBA08",
+        }
       }}
       userMenu={
-        <Tooltip title="Log Out">
-          <IconButton onClick={logout} sx={{ p: 0 }}>
-            <Avatar src="/avatar.jfif" />
-          </IconButton>
-        </Tooltip>
+        <UserMenu>
+          <Logout />
+        </UserMenu>
       }
     ></AppBar>
   );
